5f5d286e0bd721924f1d120a2d773a73a5d3d07c
jbusnte <jbustamante1302@gmail.com>
2023-10-19 22:14:06 -0400

    added patients screen with basic functionality

---
+++ app/src/main/java/com/example/gohealth/PatientsScreen.kt
@@ -0,0 +1,107 @@
+package com.example.gohealth
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gohealth.data.Patient
+import com.example.gohealth.data.PatientRepository
+import com.example.gohealth.ui.theme.GoHealthTheme
+
+@Composable
+fun PatientsList(patientRepository: PatientRepository = PatientRepository()) {
+    // State for patients
+    val patients = remember { mutableStateOf<List<Patient>?>(null) }
+    // State for loading indicator
+    val isLoading = remember { mutableStateOf(true) }
+    // State for errors
+    val error = remember { mutableStateOf<Exception?>(null) }
+
+    // Fetch patients when Composable is launched
+    LaunchedEffect(Unit) {
+        patientRepository.getAllPatients(
+            onSuccess = { fetchedPatients ->
+                patients.value = fetchedPatients
+                isLoading.value = false
+            },
+            onFailure = { e ->
+                error.value = e
+                isLoading.value = false
+            }
+        )
+    }
+
+    when {
+        isLoading.value -> {
+            // Show a loading indicator
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+        error.value != null -> {
+            // Show error message
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "Error: ${error.value?.localizedMessage}")
+            }
+        }
+        else -> {
+            // Create a Card for each Patient record
+            LazyColumn {
+                items(patients.value ?: listOf()) { patient ->
+                    PatientCard(patient = patient)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PatientCard(patient: Patient) {
+    // Create a card for the patient.
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Name: ${patient.lastName}")
+            Text(text = "Email: ${patient.email}")
+        }
+    }
+}
+
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    name = "DefaultPreviewDark"
+//)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_NO,
+//    name = "DefaultPreviewLight"
+//)
+//
+//@Composable
+//fun AppPreview() {
+//    GoHealthTheme {
+//        PatientsList()
+//    }
+//}
--- app/src/main/java/com/example/gohealth/data/Patient.kt
+++ app/src/main/java/com/example/gohealth/data/Patient.kt
@@ -1,8 +1,11 @@
 package com.example.gohealth.data
 
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
 data class Patient(
     val firstName: String,
@@ -11,7 +14,8 @@ data class Patient(
 )
 
 class PatientRepository {
-    private val db = FirebaseFirestore.getInstance()
+    private val db = Firebase.firestore
+    private val patientsCollection = FirebaseFirestore.getInstance().collection("patients")
 
     // add new patient to the Patients collection
     fun addPatient(patient: Patient) {
@@ -22,6 +26,20 @@ class PatientRepository {
             }
             .addOnFailureListener { e ->
                 println("Error adding patient: $e")
+            }
+    }
+
+    fun getAllPatients(onSuccess: (List<Patient>) -> Unit, onFailure: (Exception) -> Unit) {
+        patientsCollection.get()
+            // get all non-empty patient records & convert to Patient data class
+            .addOnSuccessListener { querySnapshot ->
+                val patients = querySnapshot.documents.mapNotNull { document ->
+                    document.toObject(Patient::class.java)
+                }
+                onSuccess(patients)
+            }
+            .addOnFailureListener { e ->
+                onFailure(e)
             }
     }
 }
